cc2e21f7aa7e7ff1caec507ac0d3fb32932a070c
Kerndog73 <kerndog73@gmail.com>
2017-11-10 17:23:30 +1030

    Add "level file.cpp"

---
+++ Buttons/Game/level file.hpp
@@ -0,0 +1,17 @@
+//
+//  level file.hpp
+//  Buttons
+//
+//  Created by Indi Kernick on 10/11/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef level_file_hpp
+#define level_file_hpp
+
+#include <string>
+#include "entity registry.hpp"
+
+void loadLevel(const std::string &, Registry &);
+
+#endif
