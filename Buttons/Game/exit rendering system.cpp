28bf523407078f3924d09ed644f5a08fc45d4266
Kerndog73 <kerndog73@gmail.com>
2017-11-14 19:34:06 +1030

    Improve exit rendering system
    It almost looks the way I want it to but not quite

--- Buttons/Game/exit rendering system.cpp
+++ Buttons/Game/exit rendering system.cpp
@@ -14,8 +14,29 @@
 #include "exit rendering component.hpp"
 
 namespace {
-  const NVGcolor LIGHT_BLUE = nvgRGBf(0.25f, 0.25f, 1.0f);
-  const NVGcolor DARK_BLUE = nvgRGBf(0.0f, 0.0f, 0.75f);
+  const NVGcolor FIRST_COLOR = nvgRGBf(0.25f, 0.25f, 1.0f);
+  const NVGcolor SECOND_COLOR = nvgRGBf(0.0f, 0.0f, 0.75f);
+  
+  void square(NVGcontext *const ctx, const NVGcolor color, const float size) {
+    static const glm::vec2 CENTER = {0.0f, 0.25f};
+    static const glm::vec2 SQUARE_SIZE = {1.0f, 0.5f};
+    
+    auto transform = [] (const float x, const float y) {
+      return glm::vec2(x, y) * SQUARE_SIZE + CENTER;
+    };
+    
+    const float halfSize = size / 2.0f;
+    
+    nvgBeginPath(ctx);
+    nvgStrokeColor(ctx, color);
+    nvgStrokeWidth(ctx, 0.1f);
+    nvgMoveTo(ctx, transform(-halfSize, -halfSize));
+    nvgLineTo(ctx, transform(-halfSize, halfSize));
+    nvgLineTo(ctx, transform(halfSize, halfSize));
+    nvgLineTo(ctx, transform(halfSize, -halfSize));
+    nvgClosePath(ctx);
+    nvgStroke(ctx);
+  }
 }
 
 void exitRenderingSystem(Registry &registry, NVGcontext *const ctx) {
@@ -26,22 +47,13 @@ void exitRenderingSystem(Registry &registry, NVGcontext *const ctx) {
     nvgTransform(ctx, getMat3(view.get<Transform>(entity)));
     
     const float prog = view.get<Animation>(entity).progress;
-    const float halfProg = prog / 2.0f;
-    const float quartProg = prog / 4.0f;
     
-    nvgBeginPath(ctx);
-    nvgFillColor(ctx, DARK_BLUE);
-    nvgRect(ctx, -0.5f, 0.0f, 1.0f, 0.5f);
-    nvgFill(ctx);
-    
-    nvgBeginPath(ctx);
-    nvgFillColor(ctx, LIGHT_BLUE);
-    nvgRect(ctx, -0.5f + halfProg, quartProg, 1.0f - prog, 0.5f - halfProg);
-    nvgFill(ctx);
-    
-    //nvgBeginPath(ctx);
-    //nvgFillColor(ctx, DARK_BLUE);
-    //nvgRect(ctx, -0.5f + 0.1f + halfProg)
+    square(ctx, FIRST_COLOR, 1.0f);
+    square(ctx, SECOND_COLOR, std::fmod(1.0f - prog + 0.8f, 1.0f));
+    square(ctx, FIRST_COLOR, std::fmod(1.0f - prog + 0.6f, 1.0f));
+    square(ctx, SECOND_COLOR, std::fmod(1.0f - prog + 0.4f, 1.0f));
+    square(ctx, FIRST_COLOR, std::fmod(1.0f - prog + 0.2f, 1.0f));
+    square(ctx, SECOND_COLOR, std::fmod(1.0f - prog + 0.0f, 1.0f));
     
     nvgRestore(ctx);
   }
