62f41cb06219df5ee531cccf39d2e384c6d64feb
Kerndog73 <kerndog73@gmail.com>
2017-11-18 14:13:47 +1030

    Add PowerInputInit and PowerOutputInit

---
+++ Buttons/Game/power init.cpp
@@ -0,0 +1,16 @@
+//
+//  power init.cpp
+//  Buttons
+//
+//  Created by Indi Kernick on 18/11/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#include "power init.hpp"
+
+#include "yaml helper.hpp"
+#include "entity id map.hpp"
+
+void PowerInputInit::init(PowerInput &comp, const YAML::Node &node, const EntityIDmap &idMap, EntityID) {
+  comp.in = idMap.getEntityFromUserID(getChild(node, "in").as<UserID>());
+}
---
+++ Buttons/Game/power init.hpp
@@ -0,0 +1,24 @@
+//
+//  power init.hpp
+//  Buttons
+//
+//  Created by Indi Kernick on 18/11/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef power_init_hpp
+#define power_init_hpp
+
+#include "comp init.hpp"
+#include "power component.hpp"
+
+class PowerInputInit : CompInit<PowerInput> {
+public:
+  PowerInputInit() = default;
+  
+  void init(PowerInput &, const YAML::Node &, const EntityIDmap &, EntityID);
+};
+
+class PowerOutputInit : CompInit<PowerOutput> {};
+
+#endif
