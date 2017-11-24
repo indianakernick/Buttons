77644b109baa7da8801caef08ba11ff439d53280
Kerndog73 <kerndog73@gmail.com>
2017-11-25 10:29:32 +1030

    Add input dispatcher

---
+++ Buttons/Game/input dispatcher.hpp
@@ -0,0 +1,18 @@
+//
+//  input dispatcher.hpp
+//  Buttons
+//
+//  Created by Indi Kernick on 25/11/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef input_dispatcher_hpp
+#define input_dispatcher_hpp
+
+#include <Simpleton/Utils/dispatcher.hpp>
+
+extern "C" union SDL_Event;
+
+using InputDispatcher = Utils::Handlable<uint32_t, SDL_Event>;
+
+#endif
