a2c620ab3757b1ce29afd3442b4d3ca8ce2649b5
Kerndog73 <kerndog73@gmail.com>
2017-11-25 09:59:39 +1030

    Add AppState enum

---
+++ Buttons/Game/state.hpp
@@ -0,0 +1,17 @@
+//
+//  state.hpp
+//  Buttons
+//
+//  Created by Indi Kernick on 25/11/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef state_hpp
+#define state_hpp
+
+enum class AppState {
+  GAME,
+  START_MENU
+};
+
+#endif
