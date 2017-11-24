e3970174a1f32fbde93320ce0b1a99113c591340
Kerndog73 <kerndog73@gmail.com>
2017-11-24 17:34:08 +1030

    Add MultiPowerInput to the list of components
    Whoops! Should have done that in the previous commit

--- Buttons/Game/component list.hpp
+++ Buttons/Game/component list.hpp
@@ -40,6 +40,7 @@
 
 #define COMPS                                                                   \
   COMP(PowerInput, powerInput)                                                  \
+  COMP(MultiPowerInput, multiPowerInput)                                        \
   COMP(PowerOutput, powerOutput)                                                \
   COMP(PhysicsBody, physicsBody)                                                \
   COMP(PhysicsJoint, physicsJoint)                                              \
