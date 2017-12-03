627478c58ca9852dde99564477edd70fe5c81647
Kerndog73 <kerndog73@gmail.com>
2017-12-03 13:03:18 +1030

    Allow the "waiting time" property of MovingPlatform to be "forever"
    I've just gotten an idea. A JSON Preprocessor (similar to the C Preprocessor). I don't have internet at the moment so I can't check if this exists. This will solve a few problems and make some of my code simpler. Instead of writing:
    
    "file": "box entity.json"
    
    I could write
    
    #include "box entity.json"

--- Buttons/Game/moving platform init.cpp
+++ Buttons/Game/moving platform init.cpp
@@ -11,6 +11,19 @@
 void MovingPlatformInit::init(MovingPlatform &comp, const json &node) {
   comp.start = node.at("start").get<b2Vec2>();
   comp.end = node.at("end").get<b2Vec2>();
-  getOptional(comp.waitingTime, node, "waiting time");
+  if (const auto waitNode = node.find("waiting time"); waitNode != node.cend()) {
+    if (waitNode->is_number()) {
+      comp.waitingTime = waitNode->get<float>();
+    } else if (waitNode->is_string()) {
+      if (waitNode->get_ref<const std::string &>() == "forever") {
+        comp.waitingTime = std::numeric_limits<float>::max();
+      } else {
+        throw std::runtime_error(
+          "Invalid value for \"waiting time\": "
+          + waitNode->dump()
+        );
+      }
+    }
+  }
   getOptional(comp.moveSpeed, node, "speed");
 }
