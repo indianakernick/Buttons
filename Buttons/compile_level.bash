a53ad78fab16b548fec03942dea176a9b45634fa
Kerndog73 <kerndog73@gmail.com>
2017-12-15 17:06:13 +1030

    Configure level compile script to compile levels into release app

--- Buttons/compile_level.bash
+++ Buttons/compile_level.bash
@@ -9,7 +9,7 @@ else
 fi
 
 PROJECT_RES="/Users/indikernick/Dev/Projects/C++/Games/Buttons/Buttons/Resources"
-PRODUCT_RES="/Users/indikernick/Library/Developer/Xcode/DerivedData/Buttons-etwygbpumdfbpjfbjwtjfrrpwvxk/Build/Products/Debug/Buttons.app/Contents/Resources"
+PRODUCT_RES="/Users/indikernick/Library/Developer/Xcode/DerivedData/Buttons-etwygbpumdfbpjfbjwtjfrrpwvxk/Build/Products/Release/Buttons.app/Contents/Resources"
 
 find $PROJECT_RES -name "level $LEVEL.jsonnet" |
 egrep --only-matching "level (\d+|final)" |
