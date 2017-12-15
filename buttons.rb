1f6639a402137b76a78a0a587210932e483505d2
Kerndog73 <kerndog73@gmail.com>
2017-12-15 16:58:00 +1030

    Create Homebrew-Cask ruby script

---
+++ buttons.rb
@@ -0,0 +1,10 @@
+cask 'buttons' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://github.com/Kerndog73/Buttons/blob/master/Buttons.zip?raw=true'
+  name 'Buttons'
+  homepage 'https://github.com/Kerndog73/Buttons'
+
+  app 'Buttons.app'
+end
